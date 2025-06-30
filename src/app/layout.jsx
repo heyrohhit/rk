44a2021a003ad3e0b33fac3c4a9554fcb7e4f92b
Rohit <101079442+Singh8700@users.noreply.github.com>
2025-06-30 19:33:56 +0530

    fixed same options

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -67,6 +67,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+      <meta name="google-site-verification" content="9h1Qm51n6eTEBXQjBEnlf9JXXnspjpIaAAgTHPCtt7w" />
+      </head>
       <body>
         <StyledJsxRegistry>
           <HeaderSection />
